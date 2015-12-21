f80ac6379b5a962fc4edb0f275d4912670de5746
tyler.ryu <tyler.ryu@daumkakao.com>
2015-12-21 23:59:24 +0900

    개발중
    Former-commit-id: e81315cae2e71f5fa393735c2957b0c1dbb3367d

--- elasticsearch/src/main/scala/org/bitbucket/eunjeon/seunjeon/elasticsearch/PosBuilder.scala
+++ elasticsearch/src/main/scala/org/bitbucket/eunjeon/seunjeon/elasticsearch/PosBuilder.scala
@@ -1,23 +1,51 @@
 package org.bitbucket.eunjeon.seunjeon.elasticsearch
 
-import org.bitbucket.eunjeon.seunjeon.{Eojeol, Analyzer}
+import com.sun.deploy.util.OrderedHashSet
+import org.bitbucket.eunjeon.seunjeon.{LNode, MorphemeType, Pos, Analyzer}
+import org.bitbucket.eunjeon.seunjeon.Pos.Pos
 import scala.collection.JavaConverters._
+import scala.collection.{mutable, SortedSet}
 
 
 object PosBuilder {
+  val indexPoses = Set[Pos](
+    Pos.N,  // 체언
+    Pos.SL, // 외국어
+    Pos.SH, // 한자
+    Pos.SN, // 숫자
+    Pos.XR, // 어근
+    Pos.V, // 용언
+    Pos.UNKNOWN)
+
   def tokenize(document:String): java.util.List[LucenePos] = {
     // TODO: 어절, 복합명사 분해
-    // TODO: 명사, 어근, 부사 등등 색인어 추출
-    // TODO: BOS, EOS 제외
     // TODO: 여러 문장의 offset 계산
-    Analyzer.parseEojeol(document).flatMap{ eojeol =>
-      eojeol.nodes.sliding(2).map{prePost =>
-        val pre = prePost.head
-        val post = prePost.last
-        // TODO: mkString 성능 괜찮을까?
-        LucenePos(1, 1, post.startPos, post.endPos, post.morpheme.surface, post.morpheme.poses.mkString("+"))
+    Analyzer.parseEojeol(document).flatMap { eojeol =>
+      var termSet = mutable.TreeSet[LucenePos]
+      termSet ++
+      eojeol.nodes.filter(isIndexNode).map { node =>
+        LucenePos(1, 1,
+          node.startPos,
+          node.endPos,
+          node.morpheme.surface,
+          node.morpheme.poses.mkString("+"))
       }
+//      if (eojeol.nodes.length > 1) {
+//      } :+ LucenePos(0, 1, eojeol.startPos, eojeol.endPos, eojeol.surface, "EOJEOL")
+//      } else {
+//        eojeol.nodes.filter(isIndexNode).map { node =>
+//          LucenePos(1, 1,
+//            node.startPos,
+//            node.endPos,
+//            node.morpheme.surface,
+//            node.morpheme.poses.mkString("+")) }
+//      }
     }.asJava
+  }
+
+  def isIndexNode(node:LNode): Boolean = {
+    node.morpheme.mType == MorphemeType.COMPOUND ||
+      (node.morpheme.mType == MorphemeType.COMMON && indexPoses.contains(node.morpheme.poses.head))
   }
 }
 
@@ -28,4 +56,22 @@ case class LucenePos(
   endOffset:Int,
   surface:String,
   poses:String
-)
+) extends Ordered[LucenePos] {
+
+  // 잘 동작하나???
+  override def compare(that: LucenePos): Int = {
+    if (startOffset == that.startOffset) {
+      if (endOffset == that.endOffset) {
+        0
+      } else if (endOffset < that.endOffset) {
+        1
+      } else {
+        -1
+      }
+    } else if (startOffset < that.startOffset) {
+      1
+    } else {
+      -1
+    }
+  }
+}
